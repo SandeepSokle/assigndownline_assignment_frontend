fd55e0742bdb7b30d8ae2d5d948892f041c5a639
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-10-27 10:15:24 +0530

    add private router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -8,13 +14,27 @@ import { useState } from "react";
 import NotFound from "./components/Nopage";
 
 function App() {
+  const isAuthenticated = () => {
+    const token = localStorage.getItem("assignTaskToken");
+    return token ? true : false;
+  };
+
+  // Private wrapper component
+  const PrivateWrapper = () => {
+    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route element={<PrivateWrapper />}>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/update" element={<Signup update={"update"} />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
